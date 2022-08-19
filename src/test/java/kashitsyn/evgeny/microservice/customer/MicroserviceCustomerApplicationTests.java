1d425ab017d77452d3395eb3b66a8db0db3970e8
ekashitsyn <ekashitsyn@technosk.ru>
2022-08-19 15:41:19 +0300

    Initial commit

---
+++ src/test/java/kashitsyn/evgeny/microservice/customer/MicroserviceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package kashitsyn.evgeny.microservice.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
